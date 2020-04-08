88a4ef675b76ed2598a2c2c0c4ec67492ad8c1b6
Philip Andresen <Philip.Andresen@gmail.com>
2020-04-08 15:38:38 -0400

    Troubleshooting codepipeline failures

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default function () {
     return (
-        <div>"HELLO WORLD" in quotes.</div>
+        <div>"HELLO WORLD" in quotes.
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
     )
 }
