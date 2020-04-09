d7a99b45cca9f6d61f0f799d91787ac210f9e24a
Philip Andresen <Philip.Andresen@gmail.com>
2020-04-08 21:16:06 -0400

    Reduced size of background image and updated style

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './styles.scss';
-import Background from '../../images/Chickadee_1.JPG';
+import Background from '../../images/Chickadee_1_a.jpg';
 
 export default function () {
     return (
         <>
             <section>
-                <article className={'header'}>
+                <div className={'header'}>
                     <div className={'text-overlay'}>
                         <div className={'text-overlay-menu'}>
                             <div className={'text-overlay-menu-item'}>Menu One</div>
@@ -16,17 +16,31 @@ export default function () {
                         <div className={'text-overlay-title'}>Maine Wildlife</div>
                     </div>
                     <div><img src={Background}/></div>
-                </article>
-                <article>
-                    <h1>Header One in an article</h1>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-                        galley of type and scrambled it to make a type specimen book. It has survived not only five
-                        centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-                        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
-                        passages, and more recently with desktop publishing software like Aldus PageMaker including
-                        versions of Lorem Ipsum.</p>
-                </article>
+                </div>
+                <div className={'article-container-container'}>
+                    <div className={'article-container'}>
+                        <article>
+                            <h1>Header One in an article</h1>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                                been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                                galley of type and scrambled it to make a type specimen book. It has survived not only five
+                                centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+                                passages, and more recently with desktop publishing software like Aldus PageMaker including
+                                versions of Lorem Ipsum.</p>
+                        </article>
+                        <article>
+                            <h1>Yet another article, haHA!</h1>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                                been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                                galley of type and scrambled it to make a type specimen book. It has survived not only five
+                                centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+                                passages, and more recently with desktop publishing software like Aldus PageMaker including
+                                versions of Lorem Ipsum.</p>
+                        </article>
+                    </div>
+                </div>
             </section>
             <footer>
                 Footer!
